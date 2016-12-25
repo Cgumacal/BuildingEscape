c690a4949dcb49873c3f14d955826b4a22a30e66
Cgumacal <chris.gumacal94@gmail.com>
2016-12-24 22:05:26 -0800

    lesson 62

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -1,35 +1,39 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BuildingEscape.h"
-#include "PositionReport.h"
+#include "OpenDoor.h"
 
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	
 	PrimaryComponentTick.bCanEverTick = true;
 
-
+	// ...
 }
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
-	Super::BeginPlay();//Start() from unity
+	Super::BeginPlay();
+	AActor *Owner = GetOwner();
+	FString ObjectName = Owner->GetName();
+
+	FRotator NewRotation = FRotator(0.0f,30.0f,0.0f);
+
+	Owner->SetActorRotation(NewRotation);
 	
-	FString ObjectPosition = GetOwner()->GetTransform().GetLocation().ToString();
-	FString ObjectName = GetOwner()->GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s is at %s!"), *ObjectName, *ObjectPosition);
+
+	//UE_LOG(LogTemp, Warning, TEXT("%s object has rotation of %s"), *ObjectName, *roat);
 	
 }
 
 
-// Called every frame Update()
-void UPositionReport::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
